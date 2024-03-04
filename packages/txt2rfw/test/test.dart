018b8788cdd91a4fd76f742cba725682d9339ff4
ebwood <timestamp.api@gmail.com>
2024-03-04 23:17:39 +0800

    txt2rfw tests

---
+++ packages/txt2rfw/test/test.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:collection/collection.dart';
+
+void main() {
+  test('txt2rfw without output path', () async {
+    Process process =
+        await Process.start('dart', ['run', 'bin/txt2rfw.dart', "test/test.rfwtxt"]);
+    await process.stdout.transform(utf8.decoder).forEach(print);
+    await process.stderr.transform(utf8.decoder).forEach(print);
+    File resultFile = File('test/match.rfw');
+    File outputFile = File('test.rfw');
+    expect(outputFile.existsSync(), true);
+    expect(
+        ListEquality()
+            .equals(resultFile.readAsBytesSync(), resultFile.readAsBytesSync()),
+        true);
+
+    outputFile.delete();
+  });
+
+  test('txt2rfw with output path', () async {
+    Process process =
+        await Process.start('dart', ['run', 'bin/txt2rfw.dart', "test/test.rfwtxt", "-o", "test.rfw"]);
+    await process.stdout.transform(utf8.decoder).forEach(print);
+    await process.stderr.transform(utf8.decoder).forEach(print);
+    File resultFile = File('test/match.rfw');
+    File outputFile = File('test.rfw');
+    expect(outputFile.existsSync(), true);
+    expect(
+        ListEquality()
+            .equals(resultFile.readAsBytesSync(), resultFile.readAsBytesSync()),
+        true);
+
+    outputFile.delete();
+  });
+}
